83118dfc13372b0d751da989e0f259ee2b58324a
Dinsul <dinsul2.0@gmail.com>
2018-10-14 13:34:47 +0300

    test_ip_filter.cpp was edited
    Test of split() function was added.
    This test check nex transforms:
     ("",  '.') -> [""]
     ("11", '.') -> ["11"]
     ("..", '.') -> ["", "", ""]
     ("11.", '.') -> ["11", ""]
     (".22", '.') -> ["", "22"]
     ("11.22", '.') -> ["11", "22"]

--- test_ip_filter.cpp
+++ test_ip_filter.cpp
@@ -2,11 +2,41 @@
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_suite_main)
+#include "ip_filter.h"
 
-BOOST_AUTO_TEST_CASE(test_allocator_valid)
+BOOST_AUTO_TEST_SUITE(test_suite_ip_filter)
+
+BOOST_AUTO_TEST_CASE(test_split)
 {
-    BOOST_CHECK(true);
+    auto runRes   = split("", '.');
+    auto rightRes = std::vector<std::string>({""});
+
+    BOOST_CHECK(runRes == rightRes);
+
+    runRes   = split("..", '.');
+    rightRes = std::vector<std::string>({"", "", ""});
+
+    BOOST_CHECK(runRes == rightRes);
+
+    runRes   = split("11.", '.');
+    rightRes = std::vector<std::string>({"11", ""});
+
+    BOOST_CHECK(runRes == rightRes);
+
+    runRes   = split("11", '.');
+    rightRes = std::vector<std::string>({"11"});
+
+    BOOST_CHECK(runRes == rightRes);
+
+    runRes   = split(".22", '.');
+    rightRes = std::vector<std::string>({"", "22"});
+
+    BOOST_CHECK(runRes == rightRes);
+
+    runRes   = split("11.22", '.');
+    rightRes = std::vector<std::string>({"11", "22"});
+
+    BOOST_CHECK(runRes == rightRes);
 }
 
 //BOOST_AUTO_TEST_CASE(test_version_valid)
